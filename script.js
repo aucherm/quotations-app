55c44c9edf17aea5b4ecfd6490f01b51466da440
Maëlle Aucher <maelle.aucher@gmail.com>
2025-11-17 10:28:10 +0100

    step5.1

--- script.js
+++ script.js
@@ -8,9 +8,27 @@ form.addEventListener('submit', function (event) {
     const text = document.getElementById('quote-text').value;
     const author = document.getElementById('quote-author').value;
 
+    addQuote(text, author);
 
-    console.log("Citation:", text);
-    console.log("Auteur.ice:", text);
 });
 
+function addQuote(quote, author) {
 
+    const pText = document.createElement('p');
+    pText.classList.add('text');
+    pText.innerText = '"' + quote + '"';
+
+    const pAuthor = document.createElement('p');
+    pAuthor.classList.add('author');
+    pAuthor.innerText = author;
+
+    const divQuote = document.createElement('div');
+    divQuote.classList.add('quote');
+    divQuote.appendChild(pText);
+    divQuote.appendChild(pAuthor);
+
+    const quoteList = document.getElementById('quote-list');
+    quoteList.appendChild(divQuote);
+}
+
+console.log(text, author);
